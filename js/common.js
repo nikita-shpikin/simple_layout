fc4f9866a69334a84180114cc2e970fab7938099
Nikita Shpikin <nikita.shpikin@gmail.com>
2022-10-20 17:22:46 +0500

    реализовал выбор отзывов с помощью jquery

--- js/common.js
+++ js/common.js
@@ -1,15 +1,25 @@
-$(function() {
-
+$(function () {
 	new WOW().init();
 
-	$(".hamburger").click(function(){
-		$(this).toggleClass("is-active");
+	$('.hamburger').click(function () {
+		$(this).toggleClass('is-active');
 
-		if($(this).hasClass('is-active')){
+		if ($(this).hasClass('is-active')) {
 			$('.mnu_top').slideDown(300);
-		}else{
+		} else {
 			$('.mnu_top').slideUp(300);
 		}
 	});
 
+	// reviews tabs
+	$('.nav_rev > div').click(function () {
+		//определить data rev id
+		const revId = $(this).data('revid');
+
+		$('.nav_rev > div').not(this).removeClass('active');
+		$(this).addClass('active');
+
+		$('.item_rev').not(revId).removeClass('active');
+		$(revId).addClass('active');
+	});
 });
